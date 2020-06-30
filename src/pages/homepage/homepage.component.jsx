aac9ac82e775d2ca6f370109f67049b0050e55f1
Chris Cuomo <chriscuomo@Chriss-MacBook-Pro.local>
2020-06-30 15:20:40 -0400

    end of section 6

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
 
-import Directory from "../../components/directory/directory.component";
-import "./homepage.styles.scss";
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.styles.scss';
 
 const HomePage = () => (
-  <div className="homepage">
+  <div className='homepage'>
     <Directory />
   </div>
 );
